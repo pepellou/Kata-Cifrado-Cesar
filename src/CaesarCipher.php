48f3382529fb6ae31580db558afdd21855d3d416
Pepe Doval <pepellou@gmail.com>
2012-12-10 21:55:09 +0100

    Cipher with offset - green

--- src/CaesarCipher.php
+++ src/CaesarCipher.php
@@ -2,10 +2,18 @@
 
 class CaesarCipher {
 
+	private $offset;
+
+	public function __construct(
+		$offset = 1
+	) {
+		$this->offset = $offset;
+	}
+
 	public function encode(
 		$aLetter
 	) {
-		return chr(ord($aLetter) + 1);
+		return chr(ord($aLetter) + $this->offset);
 	}
 
 }
