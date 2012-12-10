4632b294921174f534a10a8d75c0c1a990198cb1
Pepe Doval <pepellou@gmail.com>
2012-12-10 21:46:10 +0100

    Acceptance test

--- test/CaesarCipherTest.php
+++ test/CaesarCipherTest.php
@@ -4,4 +4,13 @@ require_once (dirname(__FILE__)."/../src/CaesarCipher.php");
 
 class CaesarCipherTest extends PHPUnit_Framework_TestCase {
 
+	public function test_acceptance(
+	) {
+		$cipher = new CaesarCipher(3);
+		$this->assertEquals(
+			"wrgr or txh vh suhjxqwded hudq odv plvpdv uhvsxhvwdv txh exvfdprv ho uhvwr gh qrvrwurv. ¿gh góqgh yhqjr? ¿d góqgh yrb? ¿fxáqwr wlhpsr whqjr? wrgr or txh sxgh kdfhu ixh vhqwduph b yhu frpr pruíd.",
+			$cipher->encode("Todo lo que se preguntaba eran las mismas respuestas que buscamos el resto de nosotros. ¿De dónde vengo? ¿A dónde voy? ¿Cuánto tiempo tengo? Todo lo que pude hacer fue sentarme y ver como moría.")
+		);
+	}
+
 }
