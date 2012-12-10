984e05da265cc95fd110368172085917e5120a99
Pepe Doval <pepellou@gmail.com>
2012-12-10 22:26:36 +0100

    Simplify API - red

--- test/CaesarCipherTest.php
+++ test/CaesarCipherTest.php
@@ -4,6 +4,13 @@ require_once (dirname(__FILE__)."/../src/CaesarCipher.php");
 
 class CaesarCipherTest extends PHPUnit_Framework_TestCase {
 
+	private $testee;
+
+	public function setUp(
+	) {
+		$this->testee = new CaesarCipher();
+	}
+
 	/**
 	* @dataProvider known_strings_ciphered
 	*/
@@ -12,10 +19,10 @@ class CaesarCipherTest extends PHPUnit_Framework_TestCase {
 		$offset,
 		$ciphered
 	) {
-		$cipher = new CaesarCipher($offset);
+		$this->testee->offset = $offset;
 		$this->assertEquals(
 			$ciphered,
-			$cipher->encode($string)
+			$this->testee->encode($string)
 		);
 	}
 
@@ -41,10 +48,10 @@ class CaesarCipherTest extends PHPUnit_Framework_TestCase {
 		$offset,
 		$ciphered
 	) {
-		$cipher = new CaesarCipher();
+		$this->testee->offset = 1;
 		$this->assertEquals(
 			$ciphered,
-			$cipher->encode($string)
+			$this->testee->encode($string)
 		);
 	}
 
@@ -62,10 +69,10 @@ class CaesarCipherTest extends PHPUnit_Framework_TestCase {
 
 	public function xtest_acceptance(
 	) {
-		$cipher = new CaesarCipher(3);
+		$this->testee->offset = 3;
 		$this->assertEquals(
 			"wrgr or txh vh suhjxqwded hudq odv plvpdv uhvsxhvwdv txh exvfdprv ho uhvwr gh qrvrwurv. ¿gh góqgh yhqjr? ¿d góqgh yrb? ¿fxáqwr wlhpsr whqjr? wrgr or txh sxgh kdfhu ixh vhqwduph b yhu frpr pruíd.",
-			$cipher->encode("Todo lo que se preguntaba eran las mismas respuestas que buscamos el resto de nosotros. ¿De dónde vengo? ¿A dónde voy? ¿Cuánto tiempo tengo? Todo lo que pude hacer fue sentarme y ver como moría.")
+			$this->testee->encode("Todo lo que se preguntaba eran las mismas respuestas que buscamos el resto de nosotros. ¿De dónde vengo? ¿A dónde voy? ¿Cuánto tiempo tengo? Todo lo que pude hacer fue sentarme y ver como moría.")
 		);
 	}
 
