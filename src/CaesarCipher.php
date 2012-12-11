307756303dd09053425fc7afd609202d55035dd9
Pepe Doval <pepellou@gmail.com>
2012-12-11 12:45:04 +0100

    Extract direction behavior

--- src/CaesarCipher.php
+++ src/CaesarCipher.php
@@ -3,39 +3,39 @@
 class CaesarCipher {
 
 	public $offset;
+	public $direction;
 
 	public function encode(
 		$aString
 	) {
-		return $this->cipher($aString, $this->offset);
+		return $this->cipher($aString, 1);
 	}
 
 	private function cipher(
 		$aString,
-		$offset
+		$direction
 	) {
-		return $this->cipherString(strtolower($aString), $offset);
+		$this->direction = $direction;
+		return $this->cipherString(strtolower($aString));
 	}
 
 	private function cipherString(
-		$aString,
-		$offset
+		$aString
 	) {
 		$ciphered = "";
 		for ($l = 0; $l < strlen($aString); $l++) {
-			$ciphered .= $this->cipherLetter($aString[$l], $offset);
+			$ciphered .= $this->cipherLetter($aString[$l]);
 		}
 		return $ciphered;
 	}
 
 	private function cipherLetter(
-		$aLetter,
-		$offset
+		$aLetter
 	) {
 		if (!$this->isAlphabeticLetter($aLetter)) {
 			return $aLetter;
 		}
-		return $this->cipherAlphabeticLetter($aLetter, $offset);
+		return $this->cipherAlphabeticLetter($aLetter);
 	}
 
 	private function isAlphabeticLetter(
@@ -46,12 +46,11 @@ class CaesarCipher {
 	}
 
 	private function cipherAlphabeticLetter(
-		$aLetter,
-		$offset
+		$aLetter
 	) {
 		$originalAscii = ord($aLetter);
 		$alphabetSize = ord('z') - ord('a') + 1;
-		$encodedAscii = $originalAscii + $offset;
+		$encodedAscii = $originalAscii + $this->direction * $this->offset;
 		$offsetFromA = ($alphabetSize + $encodedAscii - ord('a')) % $alphabetSize;
 		return chr(ord('a') + $offsetFromA);
 	}
@@ -59,7 +58,7 @@ class CaesarCipher {
 	public function decode(
 		$aString
 	) {
-		return $this->cipher($aString, -$this->offset);
+		return $this->cipher($aString, -1);
 	}
 
 }
